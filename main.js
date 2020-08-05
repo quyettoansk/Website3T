0333d4de0b2c35ac19c4edffc0831a53bbc678db
quyettoansk <58393956+quyettoansk@users.noreply.github.com>
2020-08-05 14:51:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -294,7 +294,7 @@
             });
         }
     });
-    
+
     /*==================================================================
     [ Anisto - Popup share social ]*/
     $('.share').click(function() {
@@ -328,4 +328,38 @@
         document.getElementById("coupon").innerHTML = (codeGen());
         setTimeout(setVal, 2000);
     });
+
+    // FrontPage Time Counter
+    var expiryDate = $('#counter').data('date');
+    var target = new Date(expiryDate),
+        finished = false,
+        availiableExamples = {
+            set15daysFromNow: 15 * 24 * 60 * 60 * 1000,
+            set5minFromNow: 5 * 60 * 1000,
+            set1minFromNow: 1 * 60 * 1000
+        };
+
+    function callback(event) {
+        var $this = $(this);
+        switch (event.type) {
+            case "seconds":
+            case "minutes":
+            case "hours":
+            case "days":
+            case "weeks":
+            case "daysLeft":
+                $this.find('div span#' + event.type).html(event.value);
+                if (finished) {
+                    $this.fadeTo(0, 1);
+                    finished = false;
+                }
+
+                break;
+            case "finished":
+                $this.fadeTo('slow', .5);
+                finished = true;
+                break;
+        }
+    }
+    $('#counter').countdown(target.valueOf(), callback);
 })(jQuery);
