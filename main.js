80e148f974544377db8c1c91de8b76ad9ab8789f
quyettoansk <58393956+quyettoansk@users.noreply.github.com>
2020-10-05 16:32:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 (function($) {
     "use strict";
+    
+    if ($('.accrodion-grp').length) {
+        var accrodionGrp = $('.accrodion-grp');
+        accrodionGrp.each(function () {
+            var accrodionName = $(this).data('grp-name');
+            var Self = $(this);
+            var accordion = Self.find('.accrodion');
+            Self.addClass(accrodionName);
+            Self.find('.accrodion .accrodion-content').hide();
+            Self.find('.accrodion.active').find('.accrodion-content').show();
+            accordion.each(function () {
+                $(this).find('.accrodion-title').on('click', function () {
+                    if ($(this).parent().parent().hasClass('active') === false) {
+                        $('.accrodion-grp.' + accrodionName).find('.accrodion').removeClass('active');
+                        $('.accrodion-grp.' + accrodionName).find('.accrodion').find('.accrodion-content').slideUp();
+                        $(this).parent().parent().addClass('active');
+                        $(this).parent().parent().find('.accrodion-content').slideDown();
+                    };
 
+
+                });
+            });
+        });
+
+    };
+    
     /*==================================================================
     [ Load page ]*/
     $(".animsition").animsition({
